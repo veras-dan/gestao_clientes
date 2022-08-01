4202f41c5ad3d4a702682511aaac32c68a524276
Dan <dveras1623@gmail.com>
2022-08-01 15:09:21 -0300

    Initial commit

---
+++ gestao_clientes/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for gestao_clientes project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+# from whitenoise import WhiteNoise
+# from gestao_clientes import MyWSGIApp
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_clientes.settings')
+
+application = get_asgi_application()
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root="assets")
